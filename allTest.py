ca4339b30c67c482b2d04f811b25275d58f00f36
Unknown <819163426@qq.com>
2022-08-17 18:31:09 +0800

    jenkins-test

--- allTest.py
+++ allTest.py
@@ -20,9 +20,9 @@ def run():
             "-v",
             f'--alluredir={report}/data'])
     # 自动以服务形式打开报告
-    os.system(f'allure serve {report}/data')
+    #os.system(f'allure serve {report}/data')
     # 本地生成报告
-    os.system(f'allure generate {report}/data -o {report}/html --clean')
+    #os.system(f'allure generate {report}/data -o {report}/html --clean')
 
     #docker镜像使用
     #os.system(f'allure generate {report}/data -o /usr/local/apache-tomcat-10.0.6/webapps/html --clean')
